9e10facf6a36eee5f857a3ecfd08005e9a87bf01
Erin Stalzer <erinstalzer@gmail.com>
2019-05-30 14:12:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,10 @@ class Pokemon
   attr_accessor :name, :type, :id, :db
 
 def initialize(id, name=nil, type=nil, db=nil)
+<<<<<<< HEAD
   @id = id
+=======
+>>>>>>> 4bdfcf5a9e968e24add222c55f44c40dc940e537
   #pokemon = Pokemon.find(id)
 end
 
@@ -31,6 +34,7 @@ end
       SELECT * FROM pokemon
       WHERE pokemon.id = ?
     SQL
+<<<<<<< HEAD
 
     new_pk = self.new(id)
     db.execute(sql, id).map do |row|
@@ -38,6 +42,9 @@ end
       new_pk.type = row[2]
     end
     new_pk
+=======
+    db.execute(sql, db)
+>>>>>>> 4bdfcf5a9e968e24add222c55f44c40dc940e537
   end
 
 end
